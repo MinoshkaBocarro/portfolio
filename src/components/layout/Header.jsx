92eae1c1a134ee7a8b72627b0b6afe79ef11d092
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-04-23 09:48:21 +1000

    Add header

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,40 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Divider from "../common/Divider";
+
+function Header() {
+	return (
+		<>
+			<Navbar id="navbar" className="mb-2 navbar-class" variant="dark">
+				<Container>
+					<Navbar.Brand as={Link} to="/"></Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="ms-auto navbar-custom">
+							<Nav.Link as={Link} to="/" className="px-3 fs-5">
+								Home
+							</Nav.Link>
+							<Nav.Link
+								as={Link}
+								to="/about"
+								className="px-3 fs-5"
+							>
+								About
+							</Nav.Link>
+							<Nav.Link
+								as={Link}
+								to="/projects"
+								className="px-3 fs-5"
+							>
+								Projects
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<Divider />
+		</>
+	);
+}
+
+export default Header;
