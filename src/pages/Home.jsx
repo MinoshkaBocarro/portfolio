761d6662c12e8c4dc8e02e067370a200ae64204e
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-04-23 10:05:35 +1000

    Add content to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 const Home = () => {
-	return ();
+	return (
+		<div id="hero-section">
+			<div className="intro">Hello! I&apos;m</div>
+			<h1 className="name">Minoshka Bocarro</h1>
+			<div className="subtitle">Web Developer</div>
+			<button className="button">Check Out My Projects</button>
+		</div>
+	);
 };
 
 export default Home;
