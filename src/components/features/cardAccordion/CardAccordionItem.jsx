8c72935eaba27b0d76f46c086a32bea02a506760
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-04-23 12:36:04 +1000

    Implement accordion functionality

--- src/components/features/cardAccordion/CardAccordionItem.jsx
+++ src/components/features/cardAccordion/CardAccordionItem.jsx
@@ -1,5 +1,19 @@
-function CardAccordionItem({ cardData }) {
-	return <div> Card Item {cardData}</div>;
+function CardAccordionItem({ cardData, activeIndex, onShow, onHide, index }) {
+	return (
+		<div>
+			<div onClick={onShow}>Card Item {cardData}</div>
+			{activeIndex === index ? (
+				<div>You can See Card Content</div>
+			) : (
+				<></>
+			)}
+			<button
+				onClick={activeIndex === index ? onHide : () => onShow(index)}
+			>
+				{activeIndex === index ? "Close" : " Open"}
+			</button>
+		</div>
+	);
 }
 
 export default CardAccordionItem;
--- src/components/features/cardAccordion/CardAccordionList.jsx
+++ src/components/features/cardAccordion/CardAccordionList.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+
+// components
 import CardAccordionItem from "./CardAccordionItem";
 
 function CardAccordionList() {
 	const cardList = [1, 2, 3, 4, 5, 6];
 
+	const [activeIndex, setActiveIndex] = useState(1);
+
+	function handleShow(id) {
+		console.log(id);
+		setActiveIndex(id);
+	}
+
+	function handleHide() {
+		setActiveIndex(0);
+	}
+
 	return (
 		<>
 			{cardList.map((card, index) => {
-				return <CardAccordionItem key={index} cardData={card} />;
+				return (
+					<CardAccordionItem
+						key={index}
+						cardData={card}
+						onShow={(ref) => handleShow(index, ref)}
+						onHide={() => handleHide(index)}
+						activeIndex={activeIndex}
+						index={index}
+					/>
+				);
 			})}
 		</>
 	);
