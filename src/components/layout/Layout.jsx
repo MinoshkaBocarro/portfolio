e87aecc535a56792fb94539d1ca85702e4612ac4
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-04-23 10:00:39 +1000

    Add Header and Footer to Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,23 @@
+import { Container } from "react-bootstrap";
+import Footer from "./Footer";
+import Header from "./Header";
+
+import { Outlet } from "react-router-dom";
+
 function Layout() {
-	return <></>;
+	return (
+		<div id="app">
+			<Header id="header" />
+			<div id="app-content">
+				<Container>
+					<div id="page-container">
+						<Outlet />
+					</div>
+				</Container>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default Layout;
