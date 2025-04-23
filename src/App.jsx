46e5514f80a2593b1a454880383b9e37a8ba597e
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-04-23 10:11:59 +1000

    Setup layout routing into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
+// import pages
 import Home from "./pages/Home";
+import Layout from "./components/layout/Layout";
 
 function App() {
 	return (
 		<div className="app">
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />}></Route>
+				</Route>
 			</Routes>
 		</div>
 	);
