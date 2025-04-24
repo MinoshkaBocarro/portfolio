c19e9a9eefcac1c3b0b9901dfcbd3bcb31237631
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-04-24 10:31:09 +1000

    Incorporate MbButtonLink in Home and NotFound

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import MbButtonLink from "../components/common/MbButtonLink";
+
 const Home = () => {
 	return (
 		<div id="hero-section">
 			<div className="intro">Hello! I&apos;m</div>
 			<h1 className="name">Minoshka Bocarro</h1>
 			<div className="subtitle">Web Developer</div>
-			<button className="button">Check Out My Projects</button>
+			<div className="button">
+				<MbButtonLink to={"/Projects"}>
+					Check Out My Projects
+				</MbButtonLink>
+			</div>
 		</div>
 	);
 };
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,11 @@
+import MbButtonLink from "../components/common/MbButtonLink";
+
 function NotFound() {
 	return (
 		<>
 			<h1>Not Found</h1>
 			<p>Hmmmmm, nothing here</p>
+			<MbButtonLink to={"/"}>Home </MbButtonLink>
 		</>
 	);
 }
