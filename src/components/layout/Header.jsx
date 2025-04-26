ae2e0efa0dcfc3bdc229b428b24f403d3de7026d
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-04-26 12:08:22 +1000

    Add favicons

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,21 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+// Images
+import favicon from "../../assets/favicons/favicon.svg";
+
 function Header() {
 	return (
 		<>
 			<Navbar id="navbar" className="mb-2 navbar-class" variant="dark">
 				<Container>
-					<Navbar.Brand as={Link} to="/"></Navbar.Brand>
+					<Navbar.Brand as={Link} to="/">
+						<img
+							src={favicon}
+							alt="MB Logo for Minoshka Bocarro"
+							className="favicon glow-area"
+						/>
+					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ms-auto navbar-custom">
