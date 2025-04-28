cb141cd69b1e81b2e90f2629ba466d0999ecec37
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-04-28 19:07:35 +1000

    Add forge icon pop up on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
+
+// Components
 import MbButtonLink from "../components/common/MbButtonLink";
 
+// Images
+import forgeIcon from "../assets/forge.svg";
+
 const Home = () => {
+	const [iconState, setIconState] = useState(false);
+
+	function iconOn() {
+		setIconState(true);
+	}
+
+	function iconOff() {
+		setIconState(false);
+	}
+
 	return (
 		<div id="hero-section">
 			<div className="intro">Hello! I&apos;m</div>
 			<h1 className="name">Minoshka Bocarro</h1>
 			<div className="subtitle">Web Developer</div>
 			<div className="button">
-				<MbButtonLink to={"/Projects"}>
+				{iconState && <img src={forgeIcon} alt="a bronze forge" />}
+				<MbButtonLink
+					onMouseEnter={iconOn}
+					onMouseLeave={iconOff}
+					to={"/Projects"}
+				>
 					Check Out My Projects
 				</MbButtonLink>
+				{iconState && <img src={forgeIcon} alt="a bronze forge" />}
 			</div>
 		</div>
 	);
