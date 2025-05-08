e7df324354cb484930ff86926bbdd34f1844cef5
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-05-08 13:22:05 +1000

    Format background to fill up longer pages

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,12 +2,12 @@ import Divider from "../common/Divider";
 
 function Footer() {
 	return (
-		<>
+		<div className="footer">
 			<Divider />
 			<footer className="footer-content">
 				<span>&copy; 2025 Realm Keeper ~ Minoshka Bocarro</span>
 			</footer>
-		</>
+		</div>
 	);
 }
 
