b0684ba762f6f8ae7ebc58b4e4fc6df11a071c7c
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2026-03-08 00:32:40 +1100

    Add responsiveness to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -28,28 +28,31 @@ function About() {
 				</div>
 				<div className="information">
 					<p>
-						Hello! My name is Minoshka. I am in the midst of a
-						<b> Diploma of Information Technology</b>, focusing on
-						<b> Front-End</b> and <b>Back-End</b> Web Development.
+						Hello! My name is Minoshka. I am an aspiring Web
+						Developer!
 					</p>
+
 					<p>
-						Cobbling together websites that shine in
+						Having completed my{" "}
+						<b> Diploma of Information Technology</b> in{" "}
+						<b> Front-End</b> and <b>Back-End</b> Web Development, I
+						am excited to cobble together websites that shine in
 						<span> accessibility</span> and
-						<span> user experience</span> is my passion. I love to
-						keep things organised and strive to craft
-						<span> maintainable</span> code in a smooth and
-						efficient manner.
+						<span> user experience</span>. I love to keep things
+						organised and strive to craft <span> maintainable</span>{" "}
+						code in a smooth and efficient manner.
 					</p>
 					<p>
 						With my <span>adaptability</span> and{" "}
 						<span>critical analysis</span> skills gained from my
 						studies during my <b>Bachelor of Science</b>, I am
-						confident in being able to dig deep into any website.
+						confident in being able to dig deep into the crux of any
+						website.
 					</p>
 					<p>
-						I&apos;ve always been drawn to fantasy and love almost
-						every geeky thing under the stars like board games,
-						video games and manga.
+						A little about my hobbies, I&apos;ve always been drawn
+						to fantasy and love almost every geeky thing under the
+						stars like board games, video games and manga.
 					</p>
 				</div>
 				<p className="thanks">Thank you for visiting my portfolio!</p>
